fb49451448578696123d30a6ccbb064f9b8039e6
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-01 16:11:59 -0400

    Convert Fahrenheit to Celsius git push exit q quit

---
+++ assignments/assignment-4/homework.js
@@ -0,0 +1,9 @@
+// Convert Fahrenheit to Celsius
+let fahrenheit = 75;
+let celsius = (fahrenheit - 32) * (5 / 9);
+console.log("Temperature in Celsius:", celsius);
+
+// Convert Celsius to Fahrenheit
+let celsiusTemp = 24;
+let fahrenheitTemp = celsiusTemp * (9 / 5) + 32;
+console.log("Temperature in Fahrenheit:", fahrenheitTemp);
