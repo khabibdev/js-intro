f089b0c0c4ecf1d500570bbb654d337a148827da
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-02 16:42:49 -0400

    Typo error fixed

--- assignments/assignment-4/homework-2.js
+++ assignments/assignment-4/homework-2.js
@@ -76,5 +76,5 @@ var dreamTravelDestination = "Tokyo";
 var favoriteSeason = "autumn";
 
 console.log(
-    "Hello, my name is ${firstName} and I am ${age} years old. I live in ${cityOfResidence}, where I work as a ${profession}. In my free time, I enjoy ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${favoriteColor}. My dream is to one day visit ${dreamTravelDestination}. My favorite season of the year is ${favoriteSeason} because of the beautiful changing colors of the leaves."
+    `Hello, my name is ${firstName} and I am ${age} years old. I live in ${cityOfResidence}, where I work as a ${profession}. In my free time, I enjoy ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${favoriteColor}. My dream is to one day visit ${dreamTravelDestination}. My favorite season of the year is ${favoriteSeason} because of the beautiful changing colors of the leaves.`
 );
