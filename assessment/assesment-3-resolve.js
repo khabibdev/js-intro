bb2aa8dcd01b327d9392c60facc1f1be28fc8fe9
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-04 18:15:51 -0400

    Resolve assessment-3

---
+++ assessment/assesment-3-resolve.js
@@ -0,0 +1,19 @@
+var js = "I Love Javascript";
+console.log(js.replaceAll(" ", ""));
+
+console.log("-----------------");
+var javascript = "javascript";
+var newjavascript = javascript.replace("javascript", "JavaScript");
+console.log(newjavascript);
+
+console.log("-----------------");
+
+var fullName = "John Doe";
+var initial = fullName[0] + fullName[5];
+console.log(initial);
+
+console.log("-----------------");
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var alphabetWithoutMyNameFirstCharacter = alphabet.replace("h", "");
+console.log(alphabetWithoutMyNameFirstCharacter);
