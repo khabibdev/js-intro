11cac29f6ce50a92a6f2739c06086091239fc8eb
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-09 09:14:13 -0400

    Please check my homework

---
+++ assignments/assignmet-10/homework.js
@@ -0,0 +1,38 @@
+// Between 1 and 10
+// var randomNumber = Math.floor(Math.random() * 10) + 1;
+// console.log(randomNumber);
+
+// Between 1 and 100
+// var randomNumber1 = Math.floor(Math.random() * 100) + 1;
+// console.log(randomNumber1);
+
+// Between 50 and 100
+// var randomNumber = Math.floor(Math.random() * 50) + 51;
+// console.log(randomNumber);
+
+// Between -10 and 10
+// var randomNumber = Math.floor(Math.random() * (11 + 10)) - 10;
+// console.log(randomNumber);
+
+// Between -100 and 100
+// var randomNumber = Math.floor(Math.random() * (101 + 100)) - 100;
+// console.log(randomNumber);
+
+// Between 1000 and 2000
+// var randomNumber = Math.floor(Math.random() * (2001 - 1000)) + 1000;
+// console.log(randomNumber);
+
+// Between -1000 and 0
+// var randomNumber = Math.floor(Math.random() * 1000) - 1000;
+// console.log(randomNumber);
+
+// Between 1 and 1000
+// var randomNumber = Math.floor(Math.random() * 1000) + 1;
+// console.log(randomNumber);
+
+// Between -500 and 500
+// var randomNumber = Math.floor(Math.random() * (501 + 500)) - 500;
+// console.log(randomNumber);
+// Between 10000 and 20000
+// var randomNumber = Math.floor(Math.random() * (20001 - 10000)) + 10000;
+// console.log(randomNumber);
