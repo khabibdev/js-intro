d10aeff356548f8122327ed34aef5b2623d9c3f0
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-13 18:20:20 -0400

    Checking password task is finish

---
+++ assignments/assignment-11/homework-11.js
@@ -0,0 +1,8 @@
+var password = "Correct Password";
+var correctPassword = "Correct Password";
+
+if (password === correctPassword) {
+    console.log("Access Granted");
+} else {
+    console.log("Access Denied");
+}
