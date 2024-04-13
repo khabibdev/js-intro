12bc2d3abf1a6c8163b7404597b8f5a84020d35d
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-13 13:50:55 -0400

    Finish homework-10

---
+++ assignments/assignment-9/assignment-9.js
@@ -0,0 +1,89 @@
+var ex1 = 34.5678;
+console.log(Math.round(ex1)); //35. Because 34.5678 more than half
+
+var ex2 = 456.1234;
+console.log(Math.ceil(ex2)); //457 because more than actual number
+
+var ex3 = 78.9123;
+console.log(Math.floor(ex3)); //78 because even its more than half its showing small one
+
+var ex4 = -37.6789;
+console.log(Math.round(ex4)); //-38 37.6789 its more than half
+
+var ex5 = -121.4567;
+console.log(Math.ceil(ex5)); //121 take it all extra numbers
+
+var ex6 = -84.1234;
+console.log(Math.floor(ex6)); //85 on negative it shows next number
+
+var ex7 = 3874.4999;
+console.log(Math.round(ex7)); //3874 because its less than half
+
+var ex8 = 789.01;
+console.log(Math.ceil(ex8)); //790 because .01
+
+var ex9 = 999.999;
+console.log(Math.floor(ex9)); //999 always shows small amount
+
+var ex10 = -2934.8;
+console.log(Math.round(ex10)); //-2935 because its more than half12
+
+var ex11 = -7856.1;
+console.log(Math.ceil(ex11)); //-7856 because .1
+
+var ex12 = -8745.9;
+console.log(Math.floor(ex12)); //-8746 because its negative
+
+var ex13 = 0.5534;
+console.log(Math.round(ex13)); //1 because its more than half
+
+var ex14 = 0.0167;
+console.log(Math.ceil(ex14)); //0 bigger number ????
+
+var ex15 = 0.9999;
+console.log(Math.floor(ex15)); // 0 because less than 0.9 its 0
+
+var ex16 = -0.5543;
+console.log(Math.round(ex16)); //-1 because its more than half
+
+var ex17 = -0.1234;
+console.log(Math.ceil(ex17)); //-1
+
+var ex18 = -0.9876;
+console.log(Math.floor(ex18)); //- 1 itll shows next number
+
+var ex19 = 9876.5;
+console.log(Math.round(ex19)); //9877 because its more than half
+
+var ex20 = 8765.1;
+console.log(Math.ceil(ex20)); //8766
+
+var ex21 = 7654.9;
+console.log(Math.floor(ex21)); //7654 because itll show small number
+
+var ex22 = -6543.5;
+console.log(Math.round(ex22)); //-6543 because its in the middle
+
+var ex23 = -5432.1;
+console.log(Math.ceil(ex23)); //-5432 it takes away all other numbers after .
+
+var ex24 = -4321.9;
+console.log(Math.floor(ex24)); //-4322 bugger number
+
+var ex25 = 0;
+console.log(Math.round(ex25)); //0 its 0
+
+var ex26 = 0;
+console.log(Math.ceil(ex26)); //0 its 0
+
+var ex27 = 0;
+console.log(Math.floor(ex27)); //0 its 0
+
+var ex28 = 10000;
+console.log(Math.round(ex28)); //10000 because there is no any extra numbers
+
+var ex29 = 10000;
+console.log(Math.ceil(ex29)); //10000 because there is no any extra numbers
+
+var ex30 = 10000;
+console.log(Math.floor(ex30)); //10000 because there is no any extra numbers
