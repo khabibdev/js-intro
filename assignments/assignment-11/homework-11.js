37d91de33ce57e76de877828e4929072fca00c61
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-13 18:36:24 -0400

    Finish quiz homework

--- assignments/assignment-11/homework-11.js
+++ assignments/assignment-11/homework-11.js
@@ -15,3 +15,15 @@ if (userName.length >= 5) {
 } else {
     console.log("Username must be at least 5 characters");
 }
+
+// ----   3   ---- //
+
+var question1 = "The Capitol of Great Britain?";
+var correctAnswer = "London";
+var userAnswer = "Tashkent";
+
+if (userAnswer === correctAnswer) {
+    console.log("Correct! You may proceed");
+} else {
+    console.log("Incorrect answer. Please try again");
+}
