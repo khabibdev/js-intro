b955b33274124b437a80bd7cfd1aa737733c9644
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-14 18:13:05 -0400

    Fixed typo errors

--- assessment/assessment-5.js
+++ assessment/assessment-5.js
@@ -44,17 +44,17 @@ if (count1 === count2) {
 
 // ------ 3 ------ //
 
-// var randomNumber = Math.floor(Math.random() * 1000) + 1;
+var randomNumber = Math.floor(Math.random() * 1000) + 1;
 
-// if (randomNumber / 3 === 0 && randomNumber / 5 !== 0) {
-//     console.log("Fizz");
-// } else if (randomNumber / 5 === 0 && randomNumber / 3 !== 0) {
-//     console.log("Buzz");
-// } else randomNumber / 3 === 0 && randomNumber / 5 === 0;
-// {
-//     console.log("FizzBuzz");
-//     console.log(randomNumber);
-// }
+if (randomNumber % 3 === 0 && randomNumber % 5 !== 0) {
+    console.log("Fizz");
+} else if (randomNumber % 5 === 0 && randomNumber % 3 !== 0) {
+    console.log("Buzz");
+} else randomNumber % 3 === 0 && randomNumber % 5 === 0;
+{
+    console.log("FizzBuzz");
+    console.log(randomNumber);
+}
 
 // ------ 4 ------ //
 
