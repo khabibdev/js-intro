fdfa095985b741cbeecbc42aa587a8968e9e131c
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-21 22:36:55 -0400

    Finish assignmnet-10-solution

---
+++ assignments/assignmet-10/assignment-10-solution.js
@@ -0,0 +1,29 @@
+/*---- 1 ----*/
+Math.floor(Math.random() * 10) + 1;
+
+/*---- 2 ----*/
+Math.floor(Math.random() * 100) + 1;
+
+/*---- 3 ----*/
+Math.floor(Math.random() * 51) + 50;
+
+/*---- 4 ----*/
+Math.floor(Math.random() * 21) - 10;
+
+/*---- 5 ----*/
+Math.floor(Math.random() * 201) - 100;
+
+/*---- 6 ----*/
+Math.floor(Math.random() * 1001) + 1000;
+
+/*---- 7 ----*/
+Math.floor(Math.random() * 1001) - 1000;
+
+/*---- 8 ----*/
+Math.floor(Math.random() * 1000) + 1;
+
+/*---- 9 ----*/
+Math.floor(Math.random() * 1001) - 500;
+
+/*---- 10 ----*/
+Math.floor(Math.random() * 10001) + 10000;
