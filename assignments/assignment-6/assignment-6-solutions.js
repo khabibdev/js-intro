2c14dc9bd7609c147139d6b0fcf92ce83c5720a7
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-21 20:43:02 -0400

    Finish assignmnet-6-solution

---
+++ assignments/assignment-6/assignment-6-solutions.js
@@ -0,0 +1,14 @@
+var name = "John";
+var age = 30;
+var cityOfResidence = "New York";
+var profession = "software engineer";
+var hobby1 = "reading books";
+var hobby2 = "playing the guitar";
+var favoriteFood = "pizza";
+var favoriteColor = "blue";
+var dreamTravelDestination = "Tokyo";
+var favoriteSeason = "autumn";
+
+var paragraph = `Hello, my name is ${name} and I am ${age} years old. I live in ${cityOfResidence}, where I work as a ${profession}. In my free time, I enjoy ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${favoriteColor}. My dream is to one day visit ${dreamTravelDestination}. My favorite season of the year is ${favoriteSeason} because of the beautiful changing colors of the leaves.`;
+
+console.log(paragraph);
