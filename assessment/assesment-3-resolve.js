5bc89b0920075d8f9e4b5d0ab0b1a8ce693659a9
Habibulloh Tashkandi <khabibdev@gmail.com>
2024-04-21 22:35:31 -0400

    Finish assesment3-solution

--- assessment/assesment-3-resolve.js
+++ assessment/assesment-3-resolve.js
@@ -1,51 +1,70 @@
-var removeSpace = "I Love JavaScript";
-var newRemoveSpace = removeSpace.replaceAll(" ", "");
-console.log(newRemoveSpace);
+/* ---- 1 ---- */
 
-console.log("-----------------");
+var iLoveJS = "I Love JavaScript";
+var someString = "I Love JavaScript and HTML,CSS";
+
+iLoveJS = iLoveJS.replaceAll(" ", "");
+console.log(iLoveJS);
+
+/* ---- 2 ---- */
 
 var javascript = "javascript";
 var firstSIdx = javascript.indexOf("s");
+
 var part1 = javascript[0].toUpperCase();
 var part2 = javascript.slice(1, firstSIdx);
 var part3 = javascript[firstSIdx].toUpperCase();
 var part4 = javascript.slice(firstSIdx + 1);
+
 javascript = part1 + part2 + part3 + part4;
 console.log(javascript);
 
-console.log("-----------------");
+/* ---- 3 ---- */
 
-var fullName1 = "Safarali Ramazonov";
+var fullName = "Will Smith";
+console.log(fullName1[0] + fullName1[5]); // Bad
 
-var firstNameInitial = fullName1[0];
-var spaceIdx = fullName1.indexOf(" ");
-var lastNameUnitial = fullName1[spaceIdx + 1];
-var initial = firstNameInitial + lastNameUnitial;
+var firstNameInitial = fullName[0];
+var spaceIdx = fullName.indexOf(" ");
+var lastNameInitial = fullName[spaceIdx + 1];
+
+var initial = firstNameInitial + lastNameInitial;
 console.log(initial);
 
-console.log("-----------------");
+/* ---- 4 ---- */
 
+// 1
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var nameFirstLetter = "h";
+var nameFirstLetter = "z";
 var nameFirstLetterIdx = alphabet.indexOf(nameFirstLetter);
-var alphabetWithoutMyNameFirstCharacter =
-    alphabet.slice(0, nameFirstLetterIdx) + alphabet.slice(nameFirstLetterIdx + 1);
-console.log(alphabetWithoutMyNameFirstCharacter);
 
-console.log("-----------------");
+var alphabetWithoutMyNameFirstCharacter1 =
+    alphabet.slice(0, nameFirstLetterIdx) + alphabet.slice(nameFirstLetterIdx + 1);
+
+console.log(alphabetWithoutMyNameFirstCharacter1);
+
+// 2
+var alphabetWithoutMyNameFirstCharacter2 = alphabet.replace(nameFirstLetter, "");
+console.log(alphabetWithoutMyNameFirstCharacter2);
+
+/* ---- 5 ---- */
 
 var completeSentence = "The quick brown fox jumps over the lazy dog.";
-var randomChar = "o";
-randomChar = completeSentence.indexOf(randomChar);
-var missingRandomCharString = completeSentence
-    .slice(0, +randomChar)
-    .concat(completeSentence.slice(+13));
-console.log(missingRandomCharString);
+var randomChar = "x";
+var missingRandomCharString;
+var randomCharIdx = completeSentence.indexOf(randomChar);
 
-console.log("-----------------");
+randomCharRemovedString = completeSentence
+    .slice(0, randomCharIdx)
+    .concat(completeSentence.slice(randomCharIdx + 1));
+console.log(randomCharRemovedString);
 
-var randomString = "Master Shifu is a great teacher!";
+/* ---- 6 ---- */
+
+var randomString = "I live in NY. NY is rainyaa";
 var randomChar = "a";
-randomChar = randomString.replaceAll(randomChar, "");
-randomString = randomString.length - randomChar.length;
-console.log(randomString);
+var randomStringLength = randomString.length;
+var randomStringWithoutRandomChar = randomString.replaceAll(randomChar, "");
+
+var randomCharCount = randomStringLength - randomStringWithoutRandomChar.length;
+console.log(randomCharCount);
